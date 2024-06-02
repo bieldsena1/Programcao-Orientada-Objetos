488da9964d37e402dea2d83ac0dc522f1aa3b4b7
bieldsena1 <gabrieldsenaeduardo@gmail.com>
2024-06-02 11:15:13 -0300

    Aula 07

---
+++ Aulas/Aula07/ex01/src/carta/CartaPulo.java
@@ -0,0 +1,36 @@
+public final class CartaPulo {
+    public CartaPulo(){
+        super("Pulo");
+    }
+
+    @Override
+    public void exibirInformacoes(){
+        System.out.println(String.format(
+            """
+            Nome: %s,
+            Revelada: %s
+
+            Ação: Pula a vez do jogador!
+            """
+            ,
+            super.nome
+            super.revelada;
+        ))
+
+        public void exibirInformacoes(String nome){
+            if(nome == nome){
+            """
+            Nome: %s,
+            Revelada: %s
+
+            Ação: Pula a vez do jogador!
+            """
+            ,
+            super.nome
+            super.revelada;
+
+            }
+            
+        }
+    }
+}
